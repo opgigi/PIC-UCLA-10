c395ed16dce3cd318a66e082f93f4971771379b1
opgigi <102398108+opgigi@users.noreply.github.com>
2023-01-18 15:33:29 -0800

    Create main.cpp

---
+++ Company/main.cpp
@@ -0,0 +1,15 @@
+#include"Company.h"
+#include"Employee.h"
+#include"HourlyWorker.h"
+#include"SalariedWorker.h"
+
+using namespace std;
+
+int main()
+{
+	string name;
+	string email;
+	Employee employee_0(name, email);
+
+	Company 
+}
